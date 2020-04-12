9ae6750a00ace62b750f903444f04d9313997ae5
nkmt <nfmail9@gmail.com>
2020-04-12 15:34:18 +0900

    content length validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_034205) do
+ActiveRecord::Schema.define(version: 2020_04_12_032660) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
